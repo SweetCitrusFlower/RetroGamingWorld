6b94a278d5e186a4bb13a42f064e45b089ca2f51
SweetCitrusFlower <alexhancu49@gmail.com>
2025-11-17 17:45:30 +0200

    Updated VS

--- RetroGamingWorld/obj/Release/net9.0/RetroGamingWorld.AssemblyInfo.cs
+++ RetroGamingWorld/obj/Release/net9.0/RetroGamingWorld.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetroGamingWorld")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f719e02fc33285a125d19ae0d9f6604ff9e20d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetroGamingWorld")]
