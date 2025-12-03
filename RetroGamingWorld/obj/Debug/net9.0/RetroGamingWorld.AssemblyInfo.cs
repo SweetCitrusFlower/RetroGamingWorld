21250a2437e9df42f8bc81664f54ebec65e9747e
tudororor <tudor.205.205@gmail.com>
2025-12-03 17:35:54 +0200

    Commit check after ignoring .vsidx files

--- RetroGamingWorld/obj/Debug/net9.0/RetroGamingWorld.AssemblyInfo.cs
+++ RetroGamingWorld/obj/Debug/net9.0/RetroGamingWorld.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetroGamingWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233abde43478cc88cdc21931f259270a3effaf05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9721f6a43418789d45d899c6e2073b5deff851")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetroGamingWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetroGamingWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
