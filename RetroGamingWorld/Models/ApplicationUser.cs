0b4181b650bc657fb104dbb9372d54e95cc07dbb
tudororor <tudor.205.205@gmail.com>
2026-01-04 23:08:14 +0200

    Final pull fix

--- RetroGamingWorld/Models/ApplicationUser.cs
+++ RetroGamingWorld/Models/ApplicationUser.cs
@@ -3,13 +3,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using RetroGamingWorld.Models;
 using System.ComponentModel.DataAnnotations.Schema;
 
-// PASUL 1: useri si roluri
 
 namespace RetroGamingWorld.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        // PASUL 6: useri si roluri
         public virtual ICollection<Comment> Comments { get; set; } = [];
         public virtual ICollection<Article> Articles { get; set; } = [];
         public virtual ICollection<Bookmark> Bookmarks { get; set; } = [];
